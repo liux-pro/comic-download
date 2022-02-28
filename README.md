59a1292744a5f12e59e2d0c1a5b22472e4ea00f3
Legend <me@liux.pro>
2022-03-01 03:19:12 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+#漫画下载
+从cocomanga.com下载漫画
+#libs
+puppeteer 与浏览器交互  
+puppeteer-extra 隐藏爬虫特征  
+puppeteer-extra-plugin-stealth 隐藏爬虫特征  
+
+#过程
+网站图片通过html内放一些加密参数，然后本地用加密js解密，加载图片，本来想解密图片地址的，但是有点困难，因为本地js也是加密的，看不到算法。  
+
+这样，才通过puppeteer这种东西下载漫画。
+
+#问题
+##懒加载
+图片是懒加载的，`<img>`的dom预先渲染，但是`src`为暂时为空，通过无限滚动那样懒加载。
+原来是想通过控制滚动条不停的向下滚动，滚到头再开始搞。[参考](https://github.com/chenxiaochun/blog/issues/38)
+```js
+async function autoScroll(page){
+    await page.evaluate(async () => {
+        await new Promise((resolve, reject) => {
+            var totalHeight = 0;
+            var distance = 100;
+            var timer = setInterval(() => {
+                var scrollHeight = document.body.scrollHeight;
+                window.scrollBy(0, distance);
+                totalHeight += distance;
+
+                if(totalHeight >= scrollHeight){
+                    clearInterval(timer);
+                    resolve();
+                }
+            }, 200);
+        });
+    });
+}
+```
+后来想想如果把chrome的高度设的很大，那么就不用滚动了，这就简单多了。
+```js
+page.setViewport({width: 800, height: 99999})
+```
+##
+https://github.com/puppeteer/puppeteer/issues/2176#issuecomment-434665348
---
+++ index.js
@@ -0,0 +1,78 @@
+const puppeteer = require('puppeteer-extra')
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+const fs = require('fs')
+const path = require("path")
+//延迟函数
+let sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
+//开启反-反爬虫
+puppeteer.use(StealthPlugin())
+
+
+puppeteer.launch({headless: false}).then(async browser => {
+    const comicUrl = "https://www.cocomanga.com/10101/"
+    let page = await browser.newPage();
+    await page.goto(comicUrl)
+    await page.waitForSelector(".fed-visible>.all_data_list>ul>li>a")
+    let urlList = await page.evaluate('Array.from(document.querySelectorAll(".fed-visible>.all_data_list>ul>li>a")).map(item=>{return item.href})')
+    urlList = urlList.reverse()
+    const length = urlList.length
+    await page.close()
+    for (let i = 0; i < length; i++) {
+        const url = urlList[i]
+        console.log(`下载第${i + 1}话，共${length}话,url:${url}`)
+        await comic(url.toString(), browser, i + 1)
+    }
+
+    // await comic("https://www.cocomanga.com/10101/1/920.html",browser,1)
+    await browser.close()
+})
+
+
+async function comic(url, browser, count) {
+    let page = await browser.newPage();
+    await page.setViewport({width: 800, height: 99999})
+
+    let buffers = new Map()
+    page.on('response', response => {
+        if (response.ok() && response.url().indexOf("cocomanga.com") > -1) {
+            buffers.set(response.url().toString(), response.buffer())
+        }
+    });
+    await page.goto(url)
+
+    await page.waitForSelector(".mh_comicpic img")
+    const size = await page.evaluate('document.querySelectorAll(".mh_comicpic img").length')
+    let title = await page.evaluate('document.querySelector(".mh_readtitle h1 *").innerText')
+    title = title.toString().trim()
+    const imgSrcList = await page.evaluate('Array.from(document.querySelectorAll(".mh_comicpic img")).map(item=>{return item.currentSrc})')
+
+    console.log(`下载《${title}》,共${size}页`)
+
+    if (size < 1) {
+        console.error(url)
+    }
+    const dir = `${count}@${title}`
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir)
+    }
+    for (let i = 0; i < imgSrcList.length; i++) {
+        const url = imgSrcList[i].toString()
+        //img buffer是个promise
+        let img
+        while (true) {
+            img = buffers.get(url);
+            if (img) {
+                break;
+            }
+            await sleep(300)
+        }
+        //扩展名
+        let ext = path.extname(url);
+        fs.writeFile(path.join(dir, `${i}${ext}`), await img, err => {
+            if (err) {
+                console.error(err)
+            }
+        })
+    }
+    await page.close()
+}
